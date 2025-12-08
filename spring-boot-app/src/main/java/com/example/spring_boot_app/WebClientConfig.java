44d560285dd4c181b4557a489520cae234b0f42e
Mayu Ogura(小倉 まゆ) <114968503+mayu08672@users.noreply.github.com>
2025-12-08 07:18:44 +0000

    実行できた2

--- spring-boot-app/src/main/java/com/example/spring_boot_app/WebClientConfig.java
+++ spring-boot-app/src/main/java/com/example/spring_boot_app/WebClientConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Configuration
 public class WebClientConfig {
@@ -21,4 +22,5 @@ public class WebClientConfig {
                 .defaultHeader("apikey", supabaseAnonKey)
                 .build();
     }
+   
 }
